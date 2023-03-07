2085bf601ffcb86ea28ddb71e713430cc4c464af
ClarisNjoga <mimonjoga@gmail.com>
2023-03-07 05:01:50 +0300

    react fetching data from ruby api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect, useState } from "react"
 function App() {
+
+  const [tasks, setTasks]= useState([])
+  const [users, setUsers]  = useState([])
+
+  useEffect(()=>{
+    fetch("http://localhost:9292/tasks")
+     .then(response=>response.json())
+     .then(data=>setTasks(data))
+
+    fetch("http://localhost:9292/users")
+     .then(response=>response.json())
+     .then(data=>setUsers(data))
+  },[])
+
+  const taskArray=tasks.map((task)=>{
+    return <h2>{task.name}</h2>
+  })
+  
+  const userArray=users.map((user)=>{
+    return <h2>{user.name}</h2>
+  })
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Tasks</h1>
+      {taskArray}
+      <h1>Users</h1>
+      {userArray}
     </div>
   );
 }
